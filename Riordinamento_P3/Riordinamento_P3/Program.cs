d637e2e7bb848a1d28b29469fbe6b3f7e3bc6932
gmarck04 <gmarck04@gmail.com>
2021-03-12 11:56:57 +0100

    Creazione Intestazione

---
+++ Riordinamento_P3/Riordinamento_P3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Autore: Giovanni Marchetto
+ * Data 12/03/2021
+ * Consegna: Creare un software che compia queste azioni:
+
+1) Creare un elenco di numeri interi, con possibilità di scelta da parte dell'utente se possono essere ripetuti oppure no, in un numero non minore di 123.456. (implementare i vari controlli)
+2) L'elenco in output al punto 1 verrà ordinato con algoritmo A*
+3) L'elenco in output al punto 1 verrà ordinato con algoritmo B*
+4) L'elenco in output al punto 1 verrà ordinato con algoritmo C*
+5) L'output del programma sarà la stampa a video di 3 valori, relativi alla "fatica" svolta dai 3 algoritmi utilizzati. L'ordine di presentazione sarà l'ordine crescente e al visualizzazione dovrà essere simile a:
+"Al primo posto l'algoritmo A con un punteggio di 333333.
+Al secondo posto l'algoritmo B con un punteggio di 444444.
+All terzo posto l'algoritmo C con un punteggio di 555555."
+6) Ogni algoritmo produrrà un file su cui salvare il risultato dell'ordinamento. È necessario che i 3 file di output risultino identici.
+
+________
+* A, B, C sono 3 algoritmi di ordinamento diversi tra di loro. All'interno di essi deve essere presente un "contatore" che sommi il valore dei seguenti pesi:
+1 se c'è un'operazione di "vista", come ad esempio controllare il valore di una variabile o di una posizione dell'array
+2 se c'è un'operazione di assegnazione di un nuovo valore ad una variabile
+3 se c'è un'operazione di "modifica", come ad esempio lo scambio di due variabili
+Altri pesi dovranno essere assegnati con un criterio di buonsenso.
+________
+- Non occorre presentare relazione: il codice dovrà essere necessariamente molto chiaro.
+- Giustificare eventuali ulteriori integrazioni o variazioni ai requisiti.
+- Commentare accuratamente il codice.
+- La realizzazione è da svolgere singolarmente, in autonomia.
+________
+
+La correttezza di ognuno dei 6 punti vale 1 punto.
+Ulteriori punti per:
+- scrittura corretta ed elegante del codice (1pt)
+- commenti adeguati (1pt)
+- soluzioni migliorative ed ottimizzazione (2pt)
+*/
+using System;
+
+namespace Riordinamento_P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
